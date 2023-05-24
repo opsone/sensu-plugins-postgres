f890ac370ad8876421615bc51ef4d606697417cb
Kevyn Lebouille <kevyn.lebouille@opsone.net>
2023-05-24 14:28:33 +0200

    Init

---
+++ lib/sensu/plugins/postgres.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "postgres/version"
+
+module Sensu
+  module Plugins
+    module Postgres
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
